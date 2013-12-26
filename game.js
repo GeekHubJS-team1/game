bb9536b4b97916337b8eccf1092222b61ae5ef01
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2013-12-27 00:44:25 +0200

    code autoformat, prevent user from playing in multiple tabs or browsers

--- game.js
+++ game.js
@@ -1,21 +1,21 @@
-module.exports = function(app) {
-  var io = app.get('io');
-  var online = 0;
+module.exports = function (app) {
+    var io = app.get('io');
+    var online = 0;
 
-  io.sockets.on('session:connection', function (socket) {
-    var session = socket.session;
+    io.sockets.on('session:connection', function (socket) {
+        var session = socket.session;
 
-    socket.broadcast.emit('user:in', session.user);
-    io.sockets.emit('online', ++online);
+        socket.broadcast.emit('user:in', session.user);
+        io.sockets.emit('online', ++online);
 
-    socket.on('disconnect', function () {
-      socket.broadcast.emit('user:out', session.user);
-      io.sockets.emit('online', --online);
+        socket.on('disconnect', function () {
+            socket.broadcast.emit('user:out', session.user);
+            io.sockets.emit('online', --online);
+        });
+
+        socket.on('chat', function (message) {
+            socket.broadcast.emit('chat', {user: session.user, message: message});
+            socket.emit('chat', {user: 'me', message: message});
+        });
     });
-
-    socket.on('chat', function (message) {
-      socket.broadcast.emit('chat', {user: session.user, message: message});
-      socket.emit('chat', {user: 'me', message: message});
-    });
-  });
 }
--- server.js
+++ server.js
@@ -9,42 +9,42 @@ var cookieParser = express.cookieParser('secret');
 var sessionStore = new connect.middleware.session.MemoryStore();
 
 app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(cookieParser);
-  app.use(express.session({ store: sessionStore }));
-  app.use(express.static(join(__dirname, "public"), { maxAge: 86400000 }));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(cookieParser);
+    app.use(express.session({ store: sessionStore }));
+    app.use(express.static(join(__dirname, "public"), { maxAge: 86400000 }));
 });
 
 app.post('/login', function (req, res) {
-  var login = req.body.username;
-  var pass = req.body.password;
-  if (!login || !pass) {
-    return res.send('Send login and password');
-  }
-  db.users.some(function (user){
-    if (user.login !== login) return false;
-    login = undefined; // we found login and don`t need to add it to db
-    if (user.pass !== pass) {
-      res.send('Wrong password');
-    } else {
-      req.session.user = user.login;
-      res.send('ok');
+    var login = req.body.username;
+    var pass = req.body.password;
+    if (!login || !pass) {
+        return res.send('Send login and password');
     }
-    return true;
-  });
-  if (!login) return;
-  // add user to db
-  db.users.push({
-    login: login,
-    pass: pass
-  });
-  req.session.user = login;
-  res.send('ok');
+    db.users.some(function (user) {
+        if (user.login !== login) return false;
+        login = undefined; // we found login and don`t need to add it to db
+        if (user.pass !== pass) {
+            res.send('Wrong password');
+        } else {
+            req.session.user = user.login;
+            res.send('ok');
+        }
+        return true;
+    });
+    if (!login) return;
+    // add user to db
+    db.users.push({
+        login: login,
+        pass: pass
+    });
+    req.session.user = login;
+    res.send('ok');
 });
 app.get('/logout', function (req, res) {
-  req.session.destroy();
-  res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 });
 
 var server = http.createServer(app);
@@ -54,6 +54,6 @@ app.set('io', io);
 
 require('./game')(app);
 
-server.listen(8000, function() {
-  console.log('Server listening on port ' + 8000);
+server.listen(8000, function () {
+    console.log('Server listening on port ' + 8000);
 });
