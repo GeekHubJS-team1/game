f4e27f44923c0537d8aa106b4ef0ce8e731e3639
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2014-02-14 00:11:54 +0200

    added json plugin for requirejs

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ require.config({
         'socket.io': 'vendor/socket.io',
         'kinetic': 'vendor/kinetic',
         'text': 'vendor/requirejs-text/text',
+        'json': 'vendor/requirejs-plugins/src/json',
         'slimscroll': 'vendor/jquery.slimscroll',
         'EventEmitter': 'vendor/eventEmitter/EventEmitter'
     },
