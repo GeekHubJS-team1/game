fe6e2bc086e418267457ff756b0bb718a8cf665a
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2014-02-14 10:18:11 +0200

    use mongodb to save users

--- server.js
+++ server.js
@@ -29,41 +29,11 @@ app.configure(function () {
     app.use('/'+publicDir.replace(/\\/g, '/'), express.static(publicDir));
 });
 
-app.post('/login', function (req, res) {
-    var login = req.body.username;
-    var pass = req.body.password;
-    if (!login || !pass) {
-        return res.send('Send login and password');
-    }
-    db.users.some(function (user) {
-        if (user.login !== login) return false;
-        login = undefined; // we found login and don`t need to add it to db
-        if (user.pass !== pass) {
-            res.send('Wrong password');
-        } else {
-            req.session.user = user.login;
-            res.send('ok');
-        }
-        return true;
-    });
-    if (!login) return;
-    // add user to db
-    db.users.push({
-        login: login,
-        pass: pass
-    });
-    req.session.user = login;
-    res.send('ok');
-});
-app.get('/logout', function (req, res) {
-    req.session.destroy();
-    res.redirect('/');
-});
-
 var server = http.createServer(app);
 
 var io = require('./lib/socket')(server, cookieParser, sessionStore);
 
+require('./lib/auth')(app);
 require('./lib/chat')(app, io);
 require('./lib/game')(app, io);
 
